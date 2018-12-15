d867d63a03782bbe0be10e3bc4a1f07f8ca6eabd
Laurent Colbois <laurent.colbois@gmail.com>
2018-12-15 11:04:56 +0100

    Added SNR and time_SNR computation

--- src/speech/evaluate.py
+++ src/speech/evaluate.py
@@ -7,8 +7,6 @@ def mse(speech,recovered_speech):
     return np.mean((speech-recovered_speech)**2)
 
 def time_mse(speech,recovered_speech, frame_size):
-    recovered_speech_truncated = recovered_speech[:len(speech)]
-
     list_frames,speech_padded,_,_ = data.frame_split(speech,frame_size,with_overlap=False)
     recovered_speech_padded = np.concatenate((recovered_speech_truncated,np.zeros(len(speech_padded)-len(recovered_speech_truncated))))
 
@@ -16,6 +14,23 @@ def time_mse(speech,recovered_speech, frame_size):
     time_mse = np.zeros(n_frames)
 
     for frame in range(n_frames):
-        start,end = list_frames[frame]
-        time_mse[frame] = np.mean((speech_padded[start:end]-recovered_speech_padded[start:end])**2)
+        idx = list_frames[frame]
+        time_mse[frame] = np.mean((speech_padded[idx]-recovered_speech_padded[idx])**2)
     return time_mse
+
+def SNR(noisy_speech,recovered_speech):
+    noise = noisy_speech-recovered_speech
+    return 10*np.log10(np.sum(recovered_speech**2)/np.sum(noise**2))
+
+def time_SNR(noisy_speech,recovered_speech,frame_size):
+    noise = noisy_speech-recovered_speech
+    list_frames,recovered_speech_padded,_,_ = data.frame_split(recovered_speech,frame_size,with_overlap=False)
+    noise_padded = np.concatenate((noise,np.zeros(len(recovered_speech_padded)-len(noise))))
+
+    n_frames = len(list_frames)
+    time_post_SNR = np.zeros(n_frames)
+
+    for frame in range(n_frames):
+        idx = list_frames[frame]
+        time_post_SNR[frame] = 10*np.log10(np.sum(recovered_speech_padded[idx]**2)/np.sum(noise_padded[idx]**2))
+    return time_post_SNR
